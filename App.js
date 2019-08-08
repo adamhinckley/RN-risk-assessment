e5b6b05e1650b12f3d3a4b833d36a973f6f43573
Adam Hinckley <adamhinckley@mac.com>
2019-08-07 23:45:43 -0600

    initial styling in place

--- App.js
+++ App.js
@@ -1,11 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React from "react";
 import { StyleSheet, View, Text } from "react-native";
 
@@ -20,42 +12,13 @@ const App = () => {
 };
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter
-  },
-  engine: {
-    position: "absolute",
-    right: 0
-  },
-  body: {
-    backgroundColor: Colors.white
-  },
   sectionContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
     marginTop: 32,
     paddingHorizontal: 24,
-    backgroundColor: Colors.blue
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: "600",
-    color: Colors.black
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: "400",
-    color: Colors.dark
-  },
-  highlight: {
-    fontWeight: "700"
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: "600",
-    padding: 4,
-    paddingRight: 12,
-    textAlign: "right"
+    backgroundColor: "#4286f4"
   }
 });
 
