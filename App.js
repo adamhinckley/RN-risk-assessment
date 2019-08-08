3af8a60d6064f00172a76e80f62036f1cf8956ef
Adam Hinckley <adamhinckley@mac.com>
2019-08-07 22:52:19 -0600

    init

---
+++ App.js
@@ -0,0 +1,62 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+
+import { Colors } from "react-native/Libraries/NewAppScreen";
+
+const App = () => {
+  return (
+    <View style={styles.sectionContainer}>
+      <Text>Hello</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: Colors.lighter
+  },
+  engine: {
+    position: "absolute",
+    right: 0
+  },
+  body: {
+    backgroundColor: Colors.white
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+    backgroundColor: Colors.blue
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: "600",
+    color: Colors.black
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: "400",
+    color: Colors.dark
+  },
+  highlight: {
+    fontWeight: "700"
+  },
+  footer: {
+    color: Colors.dark,
+    fontSize: 12,
+    fontWeight: "600",
+    padding: 4,
+    paddingRight: 12,
+    textAlign: "right"
+  }
+});
+
+export default App;
---
+++ android/app/src/main/java/com/risk_assessment/MainActivity.java
@@ -0,0 +1,15 @@
+package com.risk_assessment;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "risk_assessment";
+    }
+}
